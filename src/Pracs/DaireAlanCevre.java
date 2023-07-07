b993b92ec1515b4fe99feacc163b3c2178499b8e
Mehmet Uyanik <mehmetc.uyanik@gmail.com>
2023-07-07 15:15:26 +0300

    prac5 added

---
+++ src/Pracs/DaireAlanCevre.java
@@ -0,0 +1,21 @@
+package Pracs;
+
+import java.util.Scanner;
+
+public class DaireAlanCevre {
+    public static void main(String[] args) {
+
+        int r;
+        double pi=3.14;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Dairenin yarıçapını giriniz : ");
+        r=input.nextInt();
+        double alan = pi*r*r;
+        double cevre= 2*pi*r;
+
+        System.out.println("Dairenin Alanı : "+alan);
+        System.out.println("Dairenin Cevresi : "+cevre);
+    }
+}

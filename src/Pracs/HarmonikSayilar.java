43dc2ffb9a25c694190b017d5b58f53619bca794
Mehmet Uyanik <mehmetc.uyanik@gmail.com>
2023-07-08 17:28:19 +0300

    prac added

--- src/Pracs/HarmonikSayilar.java
+++ src/Pracs/HarmonikSayilar.java
@@ -1,7 +1,20 @@
 package Pracs;
 
+import java.util.Scanner;
+
 public class HarmonikSayilar {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("N Sayısını giriniz : ");
+        int n = input.nextInt();
+
+        double result = 0;
+
+        for (int i=1;i<=n;i++){
+            result+=(1.0/i);
+        }
+
+        System.out.println(result);
     }
 }

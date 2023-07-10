a59b7ba0ea3aea37ca17ab68568be4eae37a33ff
Mehmet Uyanik <mehmetc.uyanik@gmail.com>
2023-07-10 23:45:36 +0300

    task added

--- src/Tasks/UsAlma.java
+++ src/Tasks/UsAlma.java
@@ -1,19 +1,37 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class UsAlma {
 
-        static int pow(int a, int b) {
-            if(b==0) {
-                return 1;
-            }
-            else {
-                return a * pow(a,b-1);
-            }
+    static int pow(int a, int b) {
+        if(b==0) {
+            return 1;
         }
-        public static void main(String[] args) {
-
-            System.out.println( pow(2,3));
-
+        else {
+            return a * pow(a,b-1);
         }
     }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean flag=true;
+
+        while (flag) {
+            System.out.println("Taban Değeri : ");
+            int x= input.nextInt();
+            System.out.println("Üs : ");
+            int y = input.nextInt();
+            System.out.println("Sonuç : " + pow(x, y));
+
+            System.out.println("1-Yeni işlem\n2-Çıkış yap");
+            int choice = input.nextInt();
+
+            if(choice == 2){
+                System.out.println("Çıkış Yapıldı...");
+                flag=false;
+            }
+        }
+    }
+}
+
 
